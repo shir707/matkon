9538c1333ce96781171b1d961ac2d4a6501afc26
Shir Fogel <shirfogel@cs.colman.ac.il>
2021-10-29 20:05:58 +0300

    creating bundle.js

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+//Global app controller
+import num from './test'; //we exported something to the num varaible
+console.log(`I imported ${num} from another module!`);
+
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('Improted module');
+export default 23; //exporting something from a module
